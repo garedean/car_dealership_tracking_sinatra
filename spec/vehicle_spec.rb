5815ab7d1659a7a183fc4ba344b2690e06667c96
Bob Ellis & Garrett Olson <student@epicodus.com>
2015-04-29 11:01:45 -0700

    refactor to include method chaining .save method onto .new method

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -7,32 +7,28 @@ describe(Vehicle) do
   end
   describe('#make') do
     it('returns the make of a vehicle') do
-      test_vehicle = Vehicle.new('Nissan', '300zx', 2005)
-      test_vehicle.save()
+      test_vehicle = Vehicle.new('Nissan', '300zx', 2005).save()
       expect(test_vehicle.make()).to(eq('Nissan'))
     end
   end
 
   describe('#model') do
     it('returns the model of a vehicle') do
-      test_vehicle = Vehicle.new('Nissan', '300zx', 2005)
-      test_vehicle.save()
+      test_vehicle = Vehicle.new('Nissan', '300zx', 2005).save()
       expect(test_vehicle.model()).to(eq('300zx'))
     end
   end
 
   describe('#year') do
     it('returns the year of a vehicle') do
-      test_vehicle = Vehicle.new('Nissan', '300zx', 2005)
-      test_vehicle.save()
+      test_vehicle = Vehicle.new('Nissan', '300zx', 2005).save()
       expect(test_vehicle.year()).to(eq(2005))
     end
   end
 
   describe('#save') do
     it('adds a behicle to the array of saved vehicles') do
-      test_vehicle = Vehicle.new('Nissan', '300zx', 2005)
-      test_vehicle.save()
+      test_vehicle = Vehicle.new('Nissan', '300zx', 2005).save()
       expect(Vehicle.all()).to(eq([test_vehicle]))
     end
   end
@@ -43,16 +39,14 @@ describe(Vehicle) do
     end
 
     it('returns all vehicles in the array of vehicles') do
-      test_vehicle = Vehicle.new('Nissan', '300zx', 2005)
-      test_vehicle.save()
+      test_vehicle = Vehicle.new('Nissan', '300zx', 2005).save()
       expect(Vehicle.all()).to(eq([test_vehicle]))
     end
   end
 
   describe('.clear') do
     it('empties the array of vehicles') do
-      test_vehicle = Vehicle.new('Nissan', '300zx', 2005)
-      test_vehicle.save()
+      test_vehicle = Vehicle.new('Nissan', '300zx', 2005).save
       expect(Vehicle.clear()).to(eq([]))
     end
   end
@@ -66,14 +60,12 @@ describe(Vehicle) do
 
   describe('worth_buying?') do
     it("returns 'true' if vehicle is American made and less than 15 years old") do
-      test_vehicle = Vehicle.new("Dodge", "Challenger", 2015)
-      test_vehicle.save()
+      test_vehicle = Vehicle.new("Dodge", "Challenger", 2015).save()
       expect(test_vehicle.worth_buying?()).to(eq(true))
     end
 
     it("returns 'false' if vehicle is not American made and less than 15 years old") do
-      test_vehicle = Vehicle.new("Toyota", "Yaris", 2005)
-      test_vehicle.save()
+      test_vehicle = Vehicle.new("Toyota", "Yaris", 2005).save()
       expect(test_vehicle.worth_buying?()).to(eq(false))
     end
   end
