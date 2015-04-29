dca0037e6bee15496e7aec793a30f34c9fa01d7d
Bob Ellis & Garrett Olson <student@epicodus.com>
2015-04-29 10:04:15 -0700

    Test that #model returns model of car

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -9,4 +9,12 @@ describe(Vehicle) do
       expect(test_vehicle.make()).to(eq('Nissan'))
     end
   end
+
+  describe('#model') do
+    it('returns the model of a vehicle') do
+      test_vehicle = Vehicle.new('Nissan', '300zx', 2005)
+      test_vehicle.save()
+      expect(test_vehicle.model()).to(eq('300zx'))
+    end
+  end
 end
