876157f9aeb1a297c320b810c2e98dd6d8bae66d
Bob Ellis & Garrett Olson <student@epicodus.com>
2015-04-29 13:27:35 -0700

    Add #add_vehicle method which adds a vehicle to the dealership and add .find method which returns a vehicle by id

---
+++ lib/dealership.rb
@@ -0,0 +1,47 @@
+class Dealership
+  @@dealerships = []
+
+  define_method(:initialize) do |name|
+    @name = name
+    @id = @@dealerships.length().+(1)
+    @cars = []
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_method(:cars) do
+    @cars
+  end
+
+  define_method(:add_vehicle) do |vehicle|
+      @cars.push(vehicle)
+  end
+
+  define_method(:name) do
+    @name
+  end
+
+  define_method(:save) do
+    @@dealerships.push(self)
+    self
+  end
+
+  define_singleton_method(:all) do
+    @@dealerships
+  end
+
+  define_singleton_method(:clear) do
+    @@dealerships = []
+  end
+
+  define_singleton_method(:find) do |id|
+    @@dealerships.each() do |dealership|
+      if dealership.id() == id
+        return dealership
+      end
+    end
+  end
+
+end
